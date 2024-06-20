7619277aae376b67e2add348c98d523e5adfde1d
Daiane Bolzan <dainha85@gmail.com>
2024-06-20 07:53:00 -0300

    inicio do projeto spring

---
+++ src/main/java/br/com/alura/screenMatch/model/DadosSerie.java
@@ -0,0 +1,10 @@
+package br.com.alura.screenMatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias({"Title", "Titulo"}) String titulo, @JsonAlias("totalSeasons") Integer totalTemporadas, @JsonAlias("imdbRating") String avaliacao, @JsonProperty("imdbVotes") String votos) {
+
+}
